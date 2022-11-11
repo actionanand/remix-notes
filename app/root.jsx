a5d475a8722991b8761d67a89abfba545402f398
Anand raja <meetanandns@gmail.com>
2022-11-11 14:58:04 +0000

    basic error boundary

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -28,6 +29,31 @@ export default function App() {
           <MainNavigation />
         </header>
         <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({error}) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className='error'>
+          <h1>Oops! An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>Back to <Link to={'/'}> 'Home' </Link></p>
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
